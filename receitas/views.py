f9a1e960ca0a0091cc83c0ccfa3187d26c05fc75
Marcelo Barbosa <mbb.hef@uea.edu.br>
2022-03-07 18:25:18 -0400

    Nomes de receitas dinâmicas

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request,'index.html')
+
+  receitas = {
+    1: 'Sopa de Legumes',
+    2: 'Sorvete',
+    3: 'Lasanha',
+    4: 'Bolo de Chocolate',
+  }
+  dados = {
+    'nome_das_receitas': receitas
+  }
+  return render(request,'index.html', dados)
 
 def receita(request):
   return render(request, 'receita.html')
