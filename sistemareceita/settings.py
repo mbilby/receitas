34863cf4e8e842cfc29be94fa80cb0a603dfcac5
Marcelo Barbosa <mbb.hef@uea.edu.br>
2022-03-07 22:46:49 -0400

    Adicionando banco de dados POSTGRES

--- sistemareceita/settings.py
+++ sistemareceita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'sistemareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receitas',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
